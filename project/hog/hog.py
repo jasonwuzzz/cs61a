5d71f1064a6ff0c07be9f3f737758cdbff9c34ce
Jason Wu <229126195wu@gmail.com>
2023-06-17 12:28:24 +0800

    Finish Phase1 Problem5

--- project/hog/hog.py
+++ project/hog/hog.py
@@ -157,7 +157,28 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     """
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
-    "*** YOUR CODE HERE ***"
+    while True:
+        if who == 0:
+            num_rolls = strategy0(score0, score1)
+            score0 += take_turn(num_rolls, score1, dice)
+            if score0 >= goal:
+                return score0, score1
+            while extra_turn(score0, score1):
+                num_rolls = strategy0(score0, score1)
+                score0 += take_turn(num_rolls, score1, dice)
+                if score0 >= goal:
+                    return score0, score1
+        else:
+            num_rolls = strategy1(score1, score0)
+            score1 += take_turn(num_rolls, score0, dice)
+            if score1 >= goal:
+                return score0, score1
+            while extra_turn(score1, score0):
+                num_rolls = strategy1(score1, score0)
+                score1 += take_turn(num_rolls, score0, dice)
+                if score1 >= goal:
+                    return score0, score1
+        who = other(who)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
