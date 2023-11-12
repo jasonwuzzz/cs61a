9829321b94350554ee685478228c6e7fce45b3f3
Jason Wu <229126195wu@gmail.com>
2023-11-12 14:01:02 +0800

    Week 12: Proj 4 [Problem 16]

--- project/scheme/questions.scm
+++ project/scheme/questions.scm
@@ -28,7 +28,16 @@
 ;; the merged lists.
 (define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond 
+      ((null? list1)
+        list2)
+      ((null? list2)
+        list1)
+      ((comp (car list1) (car list2))
+        (cons (car list1) (merge comp (cdr list1) list2)))
+      (else
+        (cons (car list2) (merge comp list1 (cdr list2))))
+    )
   )
   ; END PROBLEM 16
 
